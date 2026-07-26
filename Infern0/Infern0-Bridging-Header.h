5c17bd6b62b376054341955734cdb0ffecb3f159
The Mystery <132539789+notthemystery@users.noreply.github.com>
2026-07-26 20:14:27 +0530

    Update Infern0-Bridging-Header.h

--- Infern0/Infern0-Bridging-Header.h
+++ Infern0/Infern0-Bridging-Header.h
@@ -1,11 +1,5 @@
 //
 // Swift bridge for Lara's VFS-backed tools.
 //
-
-#import "utils/lara/darksword.h"
-#import "utils/lara/offsets.h"
-#import "utils/lara/utils.h"
-#import "utils/lara/pe/apfs.h"
-#import "utils/lara/pe/sbx.h"
-#import "utils/lara/pe/vfs.h"
+#import "utils/lara/vfs.h"
 
